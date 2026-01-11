1478abacf646d8c520fbb1749b171d67f1e7128b
Amanda Grip <amanda.grip@stud.sti.se>
2026-01-11 11:52:50 +0100

    my version of 1.0

---
+++ src/test/java/com/krillinator/demo_5/ProductRepositoryTest.java
@@ -0,0 +1,45 @@
+package com.krillinator.demo_5;
+
+import com.krillinator.demo_5.product.Product;
+import com.krillinator.demo_5.product.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import reactor.test.StepVerifier;
+
+import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+public class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @BeforeEach
+    public void clearDatabase(){
+        productRepository.deleteAll().block();
+    }
+
+    @Test
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    void shouldSaveAndRetrieveProduct(){
+        Product product_apples = new Product(
+                null,
+                "Apples",
+                "Fresh apples from the north",
+                BigDecimal.valueOf(19.9),
+                false,
+                null
+        );
+
+        StepVerifier.create(productRepository.save(product_apples)
+                .flatMap(saved -> productRepository.findById(saved.id())))
+                .expectNextMatches(p -> p.name().equals("Apples"))
+                .verifyComplete();
+    }
+}
